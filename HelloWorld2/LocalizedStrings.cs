6da72bd6c286a4975850bb58fcf1475ff08800fd
Owe Nordquist <john.doe@elisanet.fi>
2016-02-15 08:09:15 +0200

    test2

---
+++ HelloWorld2/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorld2.Resources;
+
+namespace HelloWorld2
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
